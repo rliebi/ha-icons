a0fae94c3b675b16fee0538c48296628d4d4c056
rliebi <remo@liebi.net>
2025-07-04 23:46:19 +0200

    add initial files for Liebi Custom Icons integration

--- dist/ha-icons.js
+++ dist/ha-icons.js
@@ -4,9 +4,13 @@ const html = LitElement.prototype.html;
 class LiebiIcons extends LitElement {
   render() {
     return html`
-      <ha-iconset-svg name="valantic" size="24">
+      <ha-iconset-svg name="liebi" size="24">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M2 3h4.5l5.5 13L17.5 3H22L13.2 21h-2.5L2 3z"/>
+          <defs>
+            <g id="logo">
+              <path d="M2 3h4.5l5.5 13L17.5 3H22L13.2 21h-2.5L2 3z"/>
+            </g>
+          </defs>
         </svg>
       </ha-iconset-svg>
     `;
